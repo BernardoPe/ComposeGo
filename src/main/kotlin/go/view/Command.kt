05698adf16236d8d100744cc100de932b9667bf9
António Paulino <79115841+antonio-paulino@users.noreply.github.com>
2023-11-11 23:23:57 +0000

    Update Command.kt

--- src/main/kotlin/go/view/Command.kt
+++ src/main/kotlin/go/view/Command.kt
@@ -7,58 +7,76 @@ import java.util.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-abstract class Command(val arg : String = "") {
+
+/**
+ * Represents Commands for the User Interface.
+ */
+abstract class Command {
     open fun execute(args : List<String>, game : Board): Board = throw IllegalStateException("Game Over")
     open val isToFinish = false
 }
 
-object Play: Command("play") {
-        @OptIn(ExperimentalTime::class)
-        override fun execute(args: List<String>, game: Board): Board {
-            val time = measureTime {
-                require(game !is BoardFinish) { "Game Over" }
+
+
+/**
+ * Returns the list of commands.
+ * @param storage The Storage to load and save files from.
+ */
+fun getCommands(storage: TextFileStorage<String, Board>) : Map<String,Command> {
+    return mapOf (
+        "PLAY" to object: Command() {
+
+            /**
+             * @throws IllegalArgumentException if the game has already ended.
+             * @throws IllegalArgumentException if the provided position is invalid or has already been used.
+             */
+
+            override fun execute(args: List<String>, game: Board): Board {
+                if (game is BoardFinish) error { "Game Over" }
                 val arg = requireNotNull(args.firstOrNull()) { "Missing Position" }
                 val pos = getPosition(arg)
                 require(game.cells[pos] == null) { "Position ${arg.uppercase()} used" }
-                game.play(pos)
+                return game.play(pos)
             }
-            println(time)
-            require(game !is BoardFinish) { "Game Over" }
-            val arg = requireNotNull(args.firstOrNull()) { "Missing Position" }
-            val pos = getPosition(arg)
-            require(game.cells[pos] == null) { "Position ${arg.uppercase()} used" }
-            return game.play(pos)
-        }
 
-}
-
-fun getCommands(storage: TextFileStorage<String, Board>) : Map<String,Command> {
-    return mapOf (
-        "PLAY" to Play,
+        },
         "PASS" to object: Command() {
+
             override fun execute(args: List<String>, game : Board): Board {
                 return game.pass()
             }
+
         },
         "NEW" to object : Command() {
+
             override fun execute(args: List<String>, game : Board): Board {
                 return newBoard()
             }
+
         },
         "SAVE" to object: Command() {
+
+            /**
+             * @throws IllegalArgumentException if the name is missing or empty.
+             */
+
             override fun execute(args: List<String>, game: Board): Board{
                 require(args.isNotEmpty()) { "Missing name" }
-                requireNotNull( if (game == newBoard()) null else game ){ "Game not started" }
                 val name = args[0]
                 require(name.isNotEmpty()) { "Name must not be empty" }
                 storage.create(name, game)
                 return game
+
             }
             /**
              * @throws IllegalArgumentException if the name is missing or if the specified game is not found.
              */
         },
         "LOAD" to object: Command() {
+
+            /**
+             * @throws IllegalArgumentException if the name is missing or if the specified game is not found.
+             */
             override fun execute(args: List<String>, game : Board): Board {
                 val name = requireNotNull(args.firstOrNull()) { "Missing name" }
                 return checkNotNull(storage.read(name)) { "Game $name not found" }
