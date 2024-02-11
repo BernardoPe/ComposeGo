db22dc55f37a5cabba1bbfdcbdb885f595746903
Bernardo <tgavent3@gmail.com>
2024-02-11 12:42:44 +0000

    Final

--- src/main/kotlin/go/model/Board.kt
+++ src/main/kotlin/go/model/Board.kt
@@ -3,6 +3,9 @@ package go.model
 
 val BOARD_SIZE = BoardDimension.SMALL
 
+/**
+ * Represents the Board, with player moves.
+ */
 typealias BoardCells = Map<Position, Stone>
 
 
@@ -20,75 +23,132 @@ sealed class Board(val cells : BoardCells) {
     }
 
     /**
-     * Updates the game state. If the game has ended and this function is called, there will be an error.
-     * @return A new board with the updated game state
+     * Updates the game state.
      *
+     * @return A new board with the updated game state
+     * @throws IllegalArgumentException if the game has already ended
      */
     fun play(pos: Position) : Board {
         return when (this) {
             is BoardPass -> validatePlay(pos)
             is BoardRun -> validatePlay(pos)
-            is BoardFinish -> error("Game Over")
+            is BoardFinish -> throw IllegalStateException("Game Over")
         }
     }
 
     /**
      * Passes a turn.
-     * @return The same board if the previous turn wasn't a pass, or a Board representing the finished game state if this
+     * @return The same board if the previous turn wasn't a pass, or a Board representing the finished game state
+     * if the previous turn was a pass.
+     * @throws IllegalStateException if the game has already ended
      */
     fun pass() : Board {
         return when(this) {
             is BoardFinish -> throw IllegalStateException("Game Over")
-            is BoardPass -> BoardFinish(this.cells, calculateFinalScore(this.cells, this.currPoints))
-            is BoardRun -> BoardPass(cells, prevCells, turn.other, currPoints)
+            is BoardPass -> BoardFinish(cells, calculateFinalScore(cells, currPoints))
+            is BoardRun -> BoardPass(cells, prevCells, player.other, currPoints)
         }
     }
 
 }
 
-open class BoardRun(cells: BoardCells, val prevCells : BoardCells, val turn : Stone, var currPoints : Points<Int> ) : Board(cells) {
+/**
+ * Represents a "playing" state board, for when there are plays being made by players
+ * @property cells The moves made by players in the game
+ * @property prevCells The moves belonging to this board in the previous turn. Used for validating ko rule moves
+ * @property player This turn's player
+ * @property currPoints The current points in the game
+ */
+
+open class BoardRun(cells: BoardCells, val prevCells : BoardCells, val player : Stone, val currPoints : Points<Int> ) : Board(cells) {
     override fun equals(other: Any?): Boolean {
-        return super.equals(other) && other is BoardRun && turn == other.turn && prevCells == other.prevCells && currPoints == other.currPoints
+        return super.equals(other) && other is BoardRun && player == other.player && prevCells == other.prevCells && currPoints == other.currPoints
     }
 
+
+    /**
+     * Validates a play based on the Go rules.
+     * @param pos The played position
+     * @return A new board with the updated game state if the play was valid
+     * @throws IllegalArgumentException if the play doesn't obey the Ko or Liberty rules.
+     */
     fun validatePlay(pos: Position): Board {
 
-        var newCells = cells + (pos to turn)
-        val group = pos.getPosGroup(newCells, turn)
+        require(cells[pos] == null) { "Position used" }
 
-        group.getAdjacentGroups(newCells, turn.other).forEach{adjGroup ->
+        val newCells = cells + (pos to player)
+        val group = pos.getPosGroup(newCells, player)
+
+        var cellsAfterCaptures = newCells
+        var pointsAfterCaptures = currPoints
+
+        group.getAdjacentGroups(newCells, player.other).forEach { adjGroup ->
 
             if (!adjGroup.hasLiberties(newCells)) {
 
-                newCells = newCells - adjGroup
+                cellsAfterCaptures = cellsAfterCaptures - adjGroup
 
-                currPoints = if(turn == Stone.White)
-                    Points(currPoints.white + adjGroup.size, currPoints.black)
+                pointsAfterCaptures = if(player == Stone.WHITE)
+                    Points(pointsAfterCaptures.white + adjGroup.size, pointsAfterCaptures.black)
                 else
-                    Points(currPoints.white, currPoints.black + adjGroup.size)
+                    Points(pointsAfterCaptures.white, pointsAfterCaptures.black + adjGroup.size)
 
             }
 
         }
 
-        require(group.hasLiberties(newCells)) {"Position ${pos.String()} is not valid (liberty rule)"}
-        require(newCells != prevCells) {"Position ${pos.String()} is not valid (ko rule)"}
+        require(cellsAfterCaptures != prevCells) {"Position ${pos.String()} is not valid (ko rule)"}
+        require(group.hasLiberties(cellsAfterCaptures)) {"Position ${pos.String()} is not valid (liberty rule)"}
 
-        return if(newCells.size == BOARD_SIZE.size * BOARD_SIZE.size) BoardFinish(newCells, calculateFinalScore(cells, currPoints))
-        else BoardRun(newCells, cells, turn.other, currPoints)
+        return if(cellsAfterCaptures.size == BOARD_SIZE.size * BOARD_SIZE.size) BoardFinish(cellsAfterCaptures, calculateFinalScore(cells, pointsAfterCaptures))
+        else BoardRun(cellsAfterCaptures, cells, player.other, pointsAfterCaptures)
 
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + prevCells.hashCode()
+        result = 31 * result + player.hashCode()
+        result = 31 * result + currPoints.hashCode()
+        return result
     }
 
 }
 
+/**
+ * Removes a group from the board
+ * @param group the group to remove
+ */
 operator fun BoardCells.minus(group: Group): BoardCells = this.filterKeys { position -> position !in group.positions }.toMap()
 
+/**
+ * Represents a Board for the "passed turn" state of the game
+ * @property cells The moves made by players in the game
+ * @property prevCells The moves belonging to this board in the previous turn. Used for validating ko moves
+ * @property player This turn's player
+ * @property currPoints The current points in the game
+*/
 class BoardPass(cells : BoardCells, prevCells: BoardCells, turn: Stone, currPoints: Points<Int>) : BoardRun(cells,prevCells,turn, currPoints)
 
-class BoardFinish(cells : BoardCells, val score : Points<Float>) : Board(cells)
+/**
+ * Represents a board for the "finished" state of the game
+ * @property cells The moves made in the game
+ * @property score The final game score
+ */
+class BoardFinish(cells : BoardCells, val score : Points<Double>) : Board(cells) {
+    val winner get() = if (score.black > score.white) Stone.BLACK else if (score.black < score.white) Stone.WHITE else null
+}
 
-fun newBoard() = BoardRun(emptyMap(), emptyMap(), Stone.Black, Points(0,0))
+/**
+ * Creates a new Go Board.
+ */
+fun newBoard() = BoardRun(emptyMap(), emptyMap(), Stone.BLACK, Points(0,0))
 
+
+/**
+ * Returns the empty areas belonging to the board provided by [board]
+ * @param board The board to search for empty areas.
+ */
 fun getEmptyAreas(board: BoardCells): Set<Group> {
 
     var emptyAreas = setOf<Group>()
@@ -104,18 +164,41 @@ fun getEmptyAreas(board: BoardCells): Set<Group> {
 
 }
 
-fun getPlayerFromArea(area: Group, boardCells: BoardCells): Stone? {
+/**
+ * Returns the player that the [area] is surrounded by.
+ *
+ * If the area is not surrounded by
+ * a single player, returns null
+ *
+ * @param area The group of positions to check
+ * @param boardCells The board to search through
+ */
+fun playerFromEmptyArea(area: Group, boardCells: BoardCells): Stone? {
 
-    val whiteGroups = area.getAdjacentGroups(boardCells, Stone.White)
-    val blackGroups = area.getAdjacentGroups(boardCells, Stone.Black)
+    var adjacentPlayers = setOf<Stone>()
 
-    return if(whiteGroups.isNotEmpty() && blackGroups.isEmpty()) Stone.White
-    else if (blackGroups.isNotEmpty() && whiteGroups.isEmpty()) Stone.Black
+    for (pos in area.positions) {
+        pos.getAdjacents().forEach { adjPos ->
+            val cell = boardCells[adjPos]
+            if (cell != null) {
+                adjacentPlayers = adjacentPlayers + cell
+                if(adjacentPlayers.size == 2) return  null
+            }
+        }
+    }
+
+    return if (adjacentPlayers.size == 1) adjacentPlayers.first()
     else null
 
 }
 
-fun calculateFinalScore(cells: BoardCells, currPoints: Points<Int>): Points<Float> {
+
+/**
+ * Returns the final score of the game
+ * @param cells The board to check
+ * @param currPoints The current game points accumulated by captures
+ */
+fun calculateFinalScore(cells: BoardCells, currPoints: Points<Int>): Points<Double> {
 
     val emptyAreas = getEmptyAreas(cells)
 
@@ -123,9 +206,9 @@ fun calculateFinalScore(cells: BoardCells, currPoints: Points<Int>): Points<Floa
     var whiteTerritory = 0
 
     for (area in emptyAreas) {
-        when (getPlayerFromArea(area, cells)) {
-            Stone.Black -> blackTerritory += area.size
-            Stone.White -> whiteTerritory += area.size
+        when (playerFromEmptyArea(area, cells)) {
+            Stone.BLACK -> blackTerritory += area.size
+            Stone.WHITE -> whiteTerritory += area.size
             else -> continue
         }
     }
@@ -133,6 +216,6 @@ fun calculateFinalScore(cells: BoardCells, currPoints: Points<Int>): Points<Floa
     val blackScore = blackTerritory + currPoints.black - BOARD_SIZE.komi
     val whiteScore = whiteTerritory + currPoints.white
 
-    return Points(whiteScore.toFloat(), blackScore.toFloat())
+    return Points(whiteScore.toDouble(), blackScore)
 
 }
--- src/main/kotlin/go/view/Command.kt
+++ src/main/kotlin/go/view/Command.kt
@@ -8,6 +8,26 @@ import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
 
+
+
+/**
+ * Creates and returns the list of commands.
+ * @param storage The Storage to load and save files from.
+ * @return A map of command names to Command objects
+ */
+fun getCommands(storage: TextFileStorage<String, Board>) : Map<String,Command> {
+
+    return mapOf (
+        "PLAY" to Play,
+        "PASS" to Pass,
+        "NEW" to New,
+        "SAVE" to Save(storage),
+        "LOAD" to Load(storage),
+        "EXIT" to Exit,
+    )
+
+}
+
 /**
  * Represents Commands for the User Interface.
  */
@@ -17,73 +37,78 @@ abstract class Command {
 }
 
 
-
 /**
- * Returns the list of commands.
- * @param storage The Storage to load and save files from.
+ * @throws IllegalArgumentException if the game has already ended.
+ * @throws IllegalArgumentException if the provided position is invalid or has already been used.
  */
-fun getCommands(storage: TextFileStorage<String, Board>) : Map<String,Command> {
-    return mapOf (
-        "PLAY" to object: Command() {
+object Play : Command() {
+    override fun execute(args: List<String>, game: Board): Board {
+        if (game is BoardFinish) error { "Game Over" }
+        val arg = requireNotNull(args.firstOrNull()) { "Missing Position" }
+        val pos = getPosition(arg)
+        return game.play(pos)
+    }
 
-            /**
-             * @throws IllegalArgumentException if the game has already ended.
-             * @throws IllegalArgumentException if the provided position is invalid or has already been used.
-             */
 
-            override fun execute(args: List<String>, game: Board): Board {
-                if (game is BoardFinish) error { "Game Over" }
-                val arg = requireNotNull(args.firstOrNull()) { "Missing Position" }
-                val pos = getPosition(arg)
-                require(game.cells[pos] == null) { "Position ${arg.uppercase()} used" }
-                return game.play(pos)
-            }
-
-        },
-        "PASS" to object: Command() {
-
-            override fun execute(args: List<String>, game : Board): Board {
-                return game.pass()
-            }
-
-        },
-        "NEW" to object : Command() {
-
-            override fun execute(args: List<String>, game : Board): Board {
-                return newBoard()
-            }
-
-        },
-        "SAVE" to object: Command() {
-
-            /**
-             * @throws IllegalArgumentException if the name is missing or empty.
-             */
-
-            override fun execute(args: List<String>, game: Board): Board{
-                require(args.isNotEmpty()) { "Missing name" }
-                val name = args[0]
-                require(name.isNotEmpty()) { "Name must not be empty" }
-                storage.create(name, game)
-                return game
-
-            }
-            /**
-             * @throws IllegalArgumentException if the name is missing or if the specified game is not found.
-             */
-        },
-        "LOAD" to object: Command() {
-
-            /**
-             * @throws IllegalArgumentException if the name is missing or if the specified game is not found.
-             */
-            override fun execute(args: List<String>, game : Board): Board {
-                val name = requireNotNull(args.firstOrNull()) { "Missing name" }
-                return checkNotNull(storage.read(name)) { "Game $name not found" }
-            }
-        },
-        "EXIT" to object: Command() {
-            override val isToFinish: Boolean = true
-        }
-    )
 }
+object Pass : Command() {
+    override fun execute(args: List<String>, game : Board): Board {
+        return game.pass()
+    }
+
+}
+
+object New : Command() {
+    override fun execute(args: List<String>, game : Board): Board {
+        return newBoard()
+    }
+
+}
+
+object Load: Command() {
+
+    /**
+     * @throws IllegalArgumentException if the name is missing or if the specified game is not found.
+     */
+    private var fileStorage : TextFileStorage<String, Board>? = null
+
+    override fun execute(args: List<String>, game: Board): Board {
+        requireNotNull(fileStorage) {" Invalid storage "}
+        val name = requireNotNull(args.firstOrNull()) { "Missing name" }
+        return checkNotNull(fileStorage!!.read(name)) { "Game $name not found" }
+    }
+
+    operator fun invoke(storage: TextFileStorage<String, Board>) : Load {
+        fileStorage = storage
+        return this
+    }
+
+
+}
+object Save : Command() {
+
+    /**
+     * @throws IllegalArgumentException if the name is missing or empty.
+     */
+    private var fileStorage : TextFileStorage<String, Board>? = null
+
+    override fun execute(args: List<String>, game: Board): Board{
+        requireNotNull(fileStorage) {" Invalid storage "}
+        require(args.isNotEmpty()) { "Missing name" }
+        val name = args[0]
+        require(name.isNotEmpty()) { "Name must not be empty" }
+        fileStorage!!.create(name, game)
+        return game
+    }
+
+    operator fun invoke(storage: TextFileStorage<String, Board>) : Save {
+        fileStorage = storage
+        return this
+    }
+
+}
+
+object Exit : Command() {
+    override val isToFinish: Boolean = true
+}
+
